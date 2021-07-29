b47e90d3754106e3d1d12aa4922dbc7d682d61a3
James Hunt <jim@thetwopercent.co.uk>
2021-07-29 17:30:49 +0700

    Fix wp.editor.PlainText is deprecated since version 5.3

--- src/block/block.js
+++ src/block/block.js
@@ -13,7 +13,7 @@ import './editor.scss';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n.
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks.
-const { PlainText } = wp.editor;
+const { PlainText } = wp.blockEditor;
 
 /**
  * Register: aa Gutenberg Block.
@@ -43,15 +43,6 @@ registerBlockType( 'cvb/block-china-video-block', {
 			type: 'string',
 		},
 	},
-
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
 	edit: function( props ) {
 		const { setAttributes, attributes: { chinavid, worldvid } } = props;
 
@@ -75,15 +66,6 @@ registerBlockType( 'cvb/block-china-video-block', {
 			</div>
 		);
 	},
-
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
 	save: function( props ) {
 		const { attributes: { chinavid, worldvid } } = props;
 		return (
